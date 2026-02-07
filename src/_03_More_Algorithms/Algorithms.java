0420fa68eab68606120437f8a30f4fbf8cbd919a
nolanbooth <nb@nb.org>
2026-02-07 12:57:27 -0800

    more algorithms

--- src/_03_More_Algorithms/Algorithms.java
+++ src/_03_More_Algorithms/Algorithms.java
@@ -2,27 +2,29 @@ package _03_More_Algorithms;
 
 import java.util.List;
 
+import _01_Sorting_Algorithms.SortingVisualizer;
+
 /*
  * Write your methods in this class
  */
 public class Algorithms {
-    
-    public static String multiply(int num1, int num2) {
-        return num1 + " x " + num2 + " = " + num1*num2;
-    }
-    
-    public static int findBrokenEgg(List<String> eggs) {
-    	for(int i = 0; i < eggs.size(); i++) {
-    		if(eggs.get(i) == "cracked") {
-    			return i;
-    		}
-    	}
-        return 0;
-    }
+
+	public static String multiply(int num1, int num2) {
+		return num1 + " x " + num2 + " = " + num1 * num2;
+	}
+
+	public static int findBrokenEgg(List<String> eggs) {
+		for (int i = 0; i < eggs.size(); i++) {
+			if (eggs.get(i) == "cracked") {
+				return i;
+			}
+		}
+		return 0;
+	}
 
 	public static boolean isPrime(int i) {
-		for(int n = 2; n <= i/2; n++) {
-			if(i%n == 0) {
+		for (int n = 2; n <= i / 2; n++) {
+			if (i % n == 0) {
 				return false;
 			}
 		}
@@ -30,43 +32,101 @@ public class Algorithms {
 	}
 
 	public static boolean isSquare(int i) {
-		if(Math.sqrt(i)%1 == 0) {
+		if (Math.sqrt(i) % 1 == 0) {
 			return true;
-		}
-		else {
+		} else {
 			return false;
 		}
 	}
 
 	public static boolean isCube(int i) {
-		if(Math.cbrt(i)%1 == 0) {
+		if (Math.cbrt(i) % 1 == 0) {
 			return true;
-		}
-		else {
+		} else {
 			return false;
 		}
 	}
 
 	public static int countPearls(List<Boolean> oysters) {
-		
+
 		int pearls = 0;
-		
-		for(int i = 0; i < oysters.size(); i++) {
-    		if(oysters.get(i) == true) {
-    			pearls++;
-    		}
-    	}
+
+		for (int i = 0; i < oysters.size(); i++) {
+			if (oysters.get(i) == true) {
+				pearls++;
+			}
+		}
 		return pearls;
 	}
 
 	public static double findTallest(List<Double> peeps) {
 		double tallest = 0;
-		
-		for(int i = 0; i < peeps.size(); i++) {
-    		if(peeps.get(i) > tallest) {
-    			tallest = peeps.get(i);
-    		}
-    	}
+
+		for (int i = 0; i < peeps.size(); i++) {
+			if (peeps.get(i) > tallest) {
+				tallest = peeps.get(i);
+			}
+		}
 		return tallest;
+	}
+
+	public static Object findLongestWord(List<String> words) {
+
+		int longestWord = 0;
+		String longest = null;
+
+		for (int i = 0; i < words.size(); i++) {
+			if (words.get(i).length() > longestWord) {
+				longestWord = words.get(i).length();
+				longest = words.get(i);
+			}
+		}
+
+		return longest;
+	}
+
+	public static Object containsSOS(List<String> message1) {
+		// TODO Auto-generated method stub
+		if (message1.contains("... --- ...")) {
+			return true;
+		}
+
+		else {
+			return false;
+		}
+	}
+//fix this:
+	public static void sortScores(List<Double> results) {
+		// TODO Auto-generated method stub
+		List<Double> sorted = results;
+
+		int swaps;
+		Double temp;
+		int t = 0;
+
+		while (!(isSorted(sorted))) {
+			for (int i = 0; i < sorted.size() - 1 - t; i++) {
+				if (sorted.get(i) > sorted.get(i + 1)) {
+					temp = sorted.get(i);
+					sorted.set(i, sorted.get(i + 1));
+					sorted.set(i + 1, temp);
+				}
+			}
+
+			t++;
+		}
+		
+	
+
+	}
+
+	public static boolean isSorted(List<Double> array) {
+		for (int i = 0; i < array.size() - 1; i++) {
+			if (!(array.get(i) <= array.get(i + 1))) {
+				return false;
+			}
+		}
+
+		return true;
 	}
 }
